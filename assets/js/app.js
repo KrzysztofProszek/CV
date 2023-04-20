bfc9325f8c91e260a8a1037f7e38fee0efa9a90e
Krzysztof Proszek <kproszek@gmail.com>
2023-04-20 20:08:25 +0200

    debugged switching between sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 let sideMenuLinks = document.querySelectorAll('.side-menu a')
+let sections = document.querySelectorAll('.content .section')
 sideMenuLinks.forEach((item) => {
   item.addEventListener('click', (event) => {
     let helpClass = item.dataset.class
@@ -6,6 +7,9 @@ sideMenuLinks.forEach((item) => {
     if (!item.classList.contains('active')) {
       sideMenuLinks.forEach((item) => item.classList.remove('active'))
       item.classList.add('active')
+
+      sections.forEach((section) => section.classList.remove('active'))
+      document.querySelector(`.section.${helpClass}`).classList.add('active')
     }
 
     event.preventDefault()
